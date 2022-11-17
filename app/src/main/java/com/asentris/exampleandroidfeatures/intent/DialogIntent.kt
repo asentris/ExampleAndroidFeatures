05e67cbacbd8bea285bd26dfc3cb6158db3ab845
unknown <75403764+asentris@users.noreply.github.com>
2022-11-17 12:08:05 -0800

    Added image picker example

--- app/src/main/java/com/asentris/exampleandroidfeatures/intent/DialogIntent.kt
+++ app/src/main/java/com/asentris/exampleandroidfeatures/intent/DialogIntent.kt
@@ -1,11 +1,19 @@
 package com.asentris.exampleandroidfeatures.intent
 
+import android.app.Activity
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.PopupWindow
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.widget.SwitchCompat
 import com.asentris.exampleandroidfeatures.R
 import com.asentris.exampleandroidfeatures.core.presentation.BaseDialogFragment
@@ -16,6 +24,14 @@ class DialogIntent : BaseDialogFragment() {
 
     private var _binding: LinearLayoutBinding? = null
     private val binding get() = _binding!!
+    private lateinit var launchImagePicker: ActivityResultLauncher<Intent>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        launchImagePicker =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult())
+            { result -> showImagePopup(result) }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
@@ -39,6 +55,7 @@ class DialogIntent : BaseDialogFragment() {
                 when (item) {
                     IntentList.SEND_EMAIL -> if (isChecked) sendEmail()
                     IntentList.START_ACTIVITY -> if (isChecked) startSecondActivity()
+                    IntentList.SELECT_AND_SHOW_IMAGE -> if (isChecked) selectAndShowImage()
                 }
             }
         }
@@ -71,8 +88,34 @@ class DialogIntent : BaseDialogFragment() {
         }
     }
 
+    private fun selectAndShowImage() {
+        Intent(Intent.ACTION_GET_CONTENT).also { intent ->
+            intent.type = "image/*" // * can be replaced with jpg, png, etc.
+            launchImagePicker.launch(intent)
+        }
+    }
+
+    private fun showImagePopup(result: ActivityResult) {
+        if (result.resultCode == Activity.RESULT_OK) {
+            val uri: Uri? = result.data?.data
+            val popupView = layoutInflater.inflate(R.layout.image_view, null)
+            val wc = LinearLayout.LayoutParams.WRAP_CONTENT
+            val focusable = true // allows tap outside to dismiss
+            val popupWindow = PopupWindow(popupView, wc, wc, focusable)
+            val imageView: ImageView = popupView.findViewById(R.id.view_image)
+            imageView.setImageURI(uri)
+            popupWindow.showAtLocation(binding.root, Gravity.CENTER, 0, 0)
+            popupView.setOnClickListener { popupWindow.dismiss() }
+        }
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        launchImagePicker.unregister()
     }
 }
--- app/src/main/java/com/asentris/exampleandroidfeatures/intent/IntentList.kt
+++ app/src/main/java/com/asentris/exampleandroidfeatures/intent/IntentList.kt
@@ -2,5 +2,6 @@ package com.asentris.exampleandroidfeatures.intent
 
 enum class IntentList(val text: String) {
     SEND_EMAIL("Send email"),
-    START_ACTIVITY("Start Activity")
+    START_ACTIVITY("Start Activity"),
+    SELECT_AND_SHOW_IMAGE("Select and show image"),
 }
